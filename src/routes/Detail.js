1475ac133862a0d9f205594d26de4f3da4078845
bada308 <71962076+bada308@users.noreply.github.com>
2022-09-06 17:41:55 +0900

    movie app

---
+++ src/routes/Detail.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
+
+  const { id } = useParams();
+  const getMovies = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovieInfo(json);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+    </div>
+  );
+}
+
+export default Detail;
